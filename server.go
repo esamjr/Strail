637075d02aa38300251e6a50de36f03668b94adc
esamjr <agildimasjr@gmail.com>
2020-09-25 15:32:52 +0700

    refactor: fix server.go

--- server.go
+++ server.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
+	"os"
 	"strail/db"
 	"strail/routes"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
 	db.Init()
 	e := routes.Init()
 
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
